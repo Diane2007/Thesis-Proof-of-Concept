046455d978c030a869037d5b47e5c2c43048a775
diane2007 <wangruochong0406@gmail.com>
2023-11-03 16:30:25 -0400

    Finish the open close function.

--- Proof of Concept/Assets/Scripts/GameManager.cs
+++ Proof of Concept/Assets/Scripts/GameManager.cs
@@ -1,57 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
-    //things on the desk
-    [Header("Default Desk Objects")] public GameObject teaCup;
-    public GameObject smallBrief;
-    public GameObject closedBinder;
-    public GameObject phone;
-
-    [Header("Show only after clicked")]
-    //only when the closed binder is clicked on, the giant document opens
-    public GameObject govList;
-    public GameObject legitNews;
-    public GameObject protocols;
-
-    [Space(10)] public GameObject closeButton;
-    public GameObject protocolButton;
-    public GameObject newspaperButton;
-    public GameObject officialButton;
-
-    public static GameManager Instance;
-
+    public static GameManager instance;
     void Awake()
     {
-        if (!Instance)
+        if (!instance)
         {
             DontDestroyOnLoad(gameObject);
-            Instance = this;    //set instance to this gameObject
+            instance = this;    //set instance to this gameObject
         }
         else
         {
             Destroy(gameObject);
         }
     }
+
+    int _susLv;
+
+    //calculate player's sus level
+    public int SusLv
+    {
+        get { return _susLv; }
+        set
+        {
+            _susLv = value;
+            _susLv++;
+        }
+    }
     
     void Start()
     {
         //these things shouldn't appear at game start
-        closeButton.SetActive(false);
-        protocolButton.SetActive(false);
-        newspaperButton.SetActive(false);
-        officialButton.SetActive(false);
-        govList.SetActive(false);
-        legitNews.SetActive(false);
-        officialButton.SetActive(false);
-        protocols.SetActive(false);
-    }
-
-    public void OpenCloseDocuments()
-    {
+        InputManager.instance.CloseAll();
+        //and don't show the special protocol text
         
     }
-    
+
+    /*
+    public void OpenCloseDocuments()
+    {
+        //if player clicks on the close button
+        //close all open documents
+        if (InputManager.instance.rayHit.collider.gameObject.name == closeButton.name)
+        {
+            Debug.Log("Now hitting closeButton!");
+            CloseAll();
+        }
+        //if player clicks on the closed handbook
+        //open the protocol page and hide the closed binder
+        else if (InputManager.instance.rayHit.collider.gameObject.name == closedBinder.name)
+        {
+            Debug.Log("Now hitting closedBinder!");
+            protocols.SetActive(true);
+            closedBinder.SetActive(false);
+        }
+    }
+    */
+
+
 }
--- Proof of Concept/Assets/Scripts/inputManager.cs
+++ Proof of Concept/Assets/Scripts/inputManager.cs
@@ -5,14 +5,49 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class inputManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    Camera _mainCamera;
+    Camera mainCamera;
+    //this stores the name player clicks on
+    string rayHitName;
+    RaycastHit2D rayHit;
 
+    public static InputManager instance;
+
+    //things on the desk
+    [Header("Default Desk Objects")] public GameObject teaCup;
+    public GameObject smallBrief;
+    public GameObject closedBinder;
+    public GameObject phone;
+
+    [Header("Show only after clicked")]
+    //only when the closed binder is clicked on, the giant document opens
+    public GameObject govList;
+    public GameObject legitNews;
+    public GameObject protocols;
+
+    [Space(10)] public GameObject closeButton;
+    public GameObject protocolButton;
+    public GameObject newspaperButton;
+    public GameObject officialButton;
+
+    [Header("Text Stuff")] public GameObject specialProtocol;
+    public GameObject newsTitle, newsSubtitle, newsText, newsName;
+    
     //set main camera before game starts
     void Awake()
     {
-        _mainCamera = Camera.main;
+        mainCamera = Camera.main;
+
+        if (!instance)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     //check we are clicking a game object
@@ -25,15 +60,76 @@ public class inputManager : MonoBehaviour
         }
         
         //define and init our ray
-        RaycastHit2D rayHit = Physics2D.GetRayIntersection
-            (_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        rayHit = Physics2D.GetRayIntersection
+            (mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         
-        //if player isn't clicking on an object, do nothing
+        
+        //if player isn't clicking on a collider, do nothing
         if (!rayHit.collider)
         {
             return;
         }
         //show the object name in console
-        Debug.Log(rayHit.collider.gameObject.name);
+        rayHitName = rayHit.collider.gameObject.name;
+        Debug.Log(rayHitName);
+        DocumentControl();
+
+    }
+
+    void DocumentControl()
+    {
+        //if player clicks on the close button
+        //close all open documents and show the newspaper text
+        if (rayHitName == closeButton.name)
+        {
+            CloseAll();
+            ResetDesk();
+            
+        }
+
+        //if player clicks on the closed binder
+        //open the big binder and hide the small one
+        if (rayHitName == closedBinder.name)
+        {
+            closedBinder.SetActive(false);
+            protocols.SetActive(true);
+            specialProtocol.SetActive(true);
+            OnOffNewspaperText(false);
+            
+            //show the close button!!
+            closeButton.SetActive(true);
+        }
+    }
+
+    //are we going to show all the newspaper text? or disable all of them?
+    void OnOffNewspaperText(bool state)
+    {
+        newsSubtitle.SetActive(state);
+        newsText.SetActive(state);
+        newsTitle.SetActive(state);
+        newsName.SetActive(state);
+    }
+
+    //close all open document stuff
+    public void CloseAll()
+    {
+        closeButton.SetActive(false);
+        protocolButton.SetActive(false);
+        newspaperButton.SetActive(false);
+        officialButton.SetActive(false);
+        govList.SetActive(false);
+        legitNews.SetActive(false);
+        officialButton.SetActive(false);
+        protocols.SetActive(false);
+        specialProtocol.SetActive(false);
+    }
+
+    //show everything that's supposed to be on the desk
+    void ResetDesk()
+    {
+        smallBrief.SetActive(true);
+        closedBinder.SetActive(true);
+        //show newspaper content
+        OnOffNewspaperText(true);
     }
 }
